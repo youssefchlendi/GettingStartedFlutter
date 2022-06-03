579e37363381318b39c6a310c2c029afd9b1c064
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-03 21:12:57 +0100

    text and style

--- lib/main.dart
+++ lib/main.dart
@@ -26,38 +26,20 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: Text(this.title)), body: Center(child: MyBtn()));
-  }
-}
-
-class MyBtn extends StatelessWidget {
-  const MyBtn({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
     return Container(
-      decoration: const BoxDecoration(
-          border: Border(
-        top: BorderSide(width: 1.0, color: Color(0xFFFFFFFFFF)),
-        left: BorderSide(width: 1.0, color: Color(0xFFFFFFFFFF)),
-        right: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
-        bottom: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
-      )),
-      child: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
         decoration: const BoxDecoration(
-          border: Border(
-            top: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
-            left: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
-            right: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
-            bottom: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
-          ),
-          color: Colors.grey,
+          color: Colors.white,
         ),
-        child: const Text('OK',
-            textAlign: TextAlign.center, style: TextStyle(color: Colors.black)),
-      ),
-    );
+        padding: const EdgeInsets.all(25),
+        child: const Center(
+            child: Text(
+          'Hello world',
+          style: TextStyle(
+            color: Colors.blueGrey,
+            letterSpacing: 2.5,
+            fontSize: 20,
+          ),
+          textDirection: TextDirection.ltr,
+        )));
   }
 }
