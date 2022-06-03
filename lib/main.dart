24f1174e0cb2ccfe85306ef6982277085060cec5
mohamed youssef chlendi <youssef.chlendi@gmail.com>
2022-06-03 21:07:02 +0100

    trying containers and decoration

--- lib/main.dart
+++ lib/main.dart
@@ -27,7 +27,37 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text(this.title)),
-        body: Center(child: Image.asset("assets/dart.png")));
+        appBar: AppBar(title: Text(this.title)), body: Center(child: MyBtn()));
+  }
+}
+
+class MyBtn extends StatelessWidget {
+  const MyBtn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+          border: Border(
+        top: BorderSide(width: 1.0, color: Color(0xFFFFFFFFFF)),
+        left: BorderSide(width: 1.0, color: Color(0xFFFFFFFFFF)),
+        right: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
+        bottom: BorderSide(width: 1.0, color: Color(0xFFFF000000)),
+      )),
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 2.0),
+        decoration: const BoxDecoration(
+          border: Border(
+            top: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
+            left: BorderSide(width: 1.0, color: Color(0xFFFFDFDFDF)),
+            right: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
+            bottom: BorderSide(width: 1.0, color: Color(0xFFFF7F7F7F)),
+          ),
+          color: Colors.grey,
+        ),
+        child: const Text('OK',
+            textAlign: TextAlign.center, style: TextStyle(color: Colors.black)),
+      ),
+    );
   }
 }
